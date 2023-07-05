2e6bf4b821fb04510b03ace0cfa8348105801c6c
wuzb <wuzb@geovis.com.cn>
2023-07-05 17:31:30 +0800

    add cha26: remove duplicates from sorted array

--- cha23/substring-with-concatenation-of-all-words.go
+++ cha23/substring-with-concatenation-of-all-words.go
@@ -1,6 +1,8 @@
 package main
 
 /*
+https://leetcode.cn/problems/substring-with-concatenation-of-all-words/
+
 给定一个字符串 s 和一些长度相同的单词 words。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
 
 注意子串要与 words 中的单词完全匹配，中间不能有其他字符，但不需要考虑 words 中单词串联的顺序。
@@ -25,6 +27,8 @@ words = ["word","good","best","word"]
 /*
 思路：
 `哈希表` `双指针`
+按照words中每个单词的长度切割s，得到子字串
+
 
 
 
