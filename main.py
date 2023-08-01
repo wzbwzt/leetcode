dac45ebd8678dced72e5370ed5b07fbe44fa3b21
wuzibing <wuzb@geovis.com.cn>
2023-08-01 10:33:00 +0800

    cha52:maximum cnadies you can get from boxes

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-for i in range(1, 26):
-    print(i)                
+boxes = [1, 2, 3, 4, 5]
+status = {1: True, 2: False, 3: True, 4: False, 5: True}
+q = [i for i in boxes if status[i]]
+print(q)
 
+boxes = set(boxes)
+print(boxes)
